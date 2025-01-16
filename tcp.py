a9cec61d2afabd7cd5fedd4bf0b05aaf692bfe0c
David whyte <37925345+davdwhyte87@users.noreply.github.com>
2025-01-16 21:54:56 +0100

    vi

--- tcp.py
+++ tcp.py
@@ -5,7 +5,7 @@ host = "10.255.255.254"
 live_host = "155.138.224.183"
 port = 3000                 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+s.connect((live_host, port))
 CREATE_WALLET = b'CreateWallet\n{"address":"mello_mall","password":"12345","wallet_name":"", "vcid_username":"drake_ob", "is_vcid":false}\n0\n0\n0\n'
 TRANSFER = b'Transfer\n{"sender":"david123","receiver":"drake_ob","amount":"40000.0","transaction_id":"uiw983infH__jsknfskjs","sender_password":"12345"}\n0\n0\n0\n'
 GET_BALANCE = b'GetBalance\n{"address":"danny_f"}\n0\n0\n0\n'
@@ -15,7 +15,7 @@ GET_CHAIN_ZIP = b'GetZipChain\n0\n0\n0\n0\n'
 CREATE_USER_ID = b'CreateUserId\n{"user_name":"nino88","password":"12345"}\n0\n0\n0\n'
 VALIDATE_USER_ID = b'ValidateUserId\n{"user_name":"pamela808#","password":"123456"}\n0\n0\n0\n'
 
-s.sendall(CREATE_USER_ID)
+s.sendall(GET_BALANCE)
 data = s.recv(4000)
 
 # with open('download.zip','wb') as out:
